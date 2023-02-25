b77c2ae440ee703cff1896130eb099ec5f019d42
Rzops <kraclav@gmail.com>
2023-02-25 23:47:51 +0300

    advance component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PersonComponent } from './regForm/person/person.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AdvanceComponent } from './regForm/advance/advance.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonComponent,
+    AdvanceComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/regForm/advance/advance.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advance',
+  templateUrl: './advance.component.html',
+  styleUrls: ['./advance.component.css']
+})
+export class AdvanceComponent {
+
+}
