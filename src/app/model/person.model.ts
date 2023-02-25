dd4a30a6eb44058b23c1be13fb7202c4a0c796b2
zarar384 <fel98_98@mail.ru>
2023-02-25 21:31:03 +0100

    add person component

---
+++ src/app/model/person.model.ts
@@ -0,0 +1,9 @@
+import { Advance } from "./advance.model";
+
+export interface Person {
+    mail: string;
+    phone: number;
+    sex: boolean;
+    advadce: Advance;
+    dr: Date; //date registration
+}
