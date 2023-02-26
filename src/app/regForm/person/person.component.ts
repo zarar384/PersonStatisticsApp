f73707ac993ab68f46a5486297ba2f4b817adc9e
zarar384 <fel98_98@mail.ru>
2023-02-26 21:16:56 +0100

    fix error person component

--- src/app/regForm/person/person.component.ts
+++ src/app/regForm/person/person.component.ts
@@ -6,61 +6,80 @@ import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
+  styleUrls: ['./person.component.css'],
 })
 export class PersonComponent implements OnInit {
   isAdvance: boolean = false;
-  otherCheck: string = "Other";
+  otherCheck: string = 'Other';
   person: Person = new Person();
 
-  constructor(private apiHit: ApiService){}
+  constructor(private apiHit: ApiService) {
+    this.form;
+  }
 
-  ngOnInit(){
+  ngOnInit() {
     // this.apiHit.getData().subscribe((response:any) => {
     //   this.person = response;
     //   console.log(this.person);
     // })
   }
-  integreRegex = /^\d+$/
+  integreRegex = /^\d+$/;
 
-  emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+  emailRegex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-  registerForm = new FormGroup({
-    name : new FormControl("", [Validators.required, Validators.maxLength(32)]),
-    phone : new FormControl("", [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern(this.integreRegex)]),
-    email : new FormControl("", [Validators.required, Validators.maxLength(32), Validators.pattern(this.emailRegex)]),
-    sex : new FormControl("", [Validators.required, Validators.maxLength(32)]),
-  })
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(32)]),
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+      Validators.pattern(this.integreRegex),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(32),
+      Validators.pattern(this.emailRegex),
+    ]),
+    sex: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(32),
+      this.clickOtherRadio,
+    ]),
+  });
 
-  registerFn(){
-    console.log(this.registerForm.value)
-    this.person.name = this.registerForm.value.name as string;
-    this.person.phone = this.registerForm.value.phone as unknown as number;
-    this.person.mail = this.registerForm.value.email as string;
-    this.person.sex = this.registerForm.value.sex as string;
-    
-    this.apiHit.postData(this.person).subscribe((response:any) => {
-      console.log("Data inserted successfully");
+  registerFn() {
+    console.log(this.form.value);
+    this.person.name = this.form.value.name as string;
+    this.person.phone = this.form.value.phone as unknown as number;
+    this.person.mail = this.form.value.email as string;
+    this.person.sex = this.form.value.sex as string;
+
+    this.apiHit.postData(this.person).subscribe((response: any) => {
+      console.log('Data inserted successfully');
       this.ngOnInit();
-    })
-
+    });
   }
 
-  withAdvance(value: boolean){
-    this.isAdvance = this.isAdvance == false? true : false;
+  withAdvance(value: boolean) {
+    this.isAdvance = this.isAdvance == false ? true : false;
   }
 
-  clickOtherRadio(event: MouseEvent){
-    if(event){      
-      this.otherCheck = "Eblan";
+  clickOtherRadio(control: FormControl) {
+    if (control.value == 'eblan') {
+      return {
+        otherSelected: true,
+      };
     }
+    return null;
   }
 
-  clickCorrectRadio(event: MouseEvent){
-    if(event){
-      if(this.otherCheck==="Eblan"){
-        this.otherCheck = "Other";
-      }
+  clickCorrectRadio(event: MouseEvent) {
+    const getOtherErr = this.form.get('sex')?.errors?.['otherSelected'];
+    if (getOtherErr === true || getOtherErr === undefined) {
+      this.otherCheck = 'Eblan';
+    } else {
+      this.otherCheck = 'Other';
     }
   }
 }
