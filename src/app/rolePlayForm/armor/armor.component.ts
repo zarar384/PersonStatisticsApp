411ee348b3e7bb9dcbd128f56d2bd9aae60d4f80
Rzops <kraclav@gmail.com>
2023-03-17 18:27:36 +0300

    Rp-components: ability, armor

---
+++ src/app/rolePlayForm/armor/armor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-armor',
+  templateUrl: './armor.component.html',
+  styleUrls: ['./armor.component.css']
+})
+export class armorComponent {
+
+}
